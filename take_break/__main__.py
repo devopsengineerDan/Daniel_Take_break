c5984c34c272e43546e3c47a8cbb7dad791dc79e
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-08-05 21:28:50 +0300

    made deployable

--- take_break/__main__.py
+++ take_break/__main__.py
@@ -6,40 +6,7 @@ import click
 import six
 from PyInquirer import (Token, ValidationError, Validator, print_json, prompt,
                         style_from_dict)
-
 from pyfiglet import figlet_format
-
-print('This program started on' + time.ctime())
-
-
-def take_break():
-    print('one break comes after two hours\n')
-    num_breaks = int(input('how many breaks do you want?: '))
-    break_duaration = int(
-        input('After how many hours do you need the break?: '))
-    breaks = 0
-    while (breaks < num_breaks):
-        uin = break_duaration
-        when_to_stop = abs(int(uin))
-
-        while when_to_stop >= 0:
-            m, s = divmod(when_to_stop, 60)
-            h, m = divmod(m, 60)
-            time_left = str(h).zfill(2) + ":" + \
-                str(m).zfill(2) + ":" + str(s).zfill(2)
-            print(time_left + "\r", end="")
-            time.sleep(1)
-            when_to_stop -= 1
-            while when_to_stop == 0:
-                accept = input("accept the break(Y/N): ")
-                if accept.upper() == 'Y':
-                    print("when done with break hit enter\n")
-                    webbrowser.open(
-                        "https://www.youtube.com/watch?v=m7Bc3pLyij0",
-                        new=0, autoraise=True)
-                else:
-                    break
-    breaks += 1
 
 
 try:
@@ -93,6 +60,17 @@ class Validator(Validator):
                 cursor_position=len(email.text))
 
 
+def confirm():
+    questions = [
+        {
+            'type': 'confirm',
+            'name': 'send',
+            'message': 'Accept the break?'
+        }
+    ]
+    answers = prompt(questions, style=style)
+    return answers
+
 def inPuts():
     questions = [
         {
@@ -110,7 +88,7 @@ def inPuts():
         {
             'type': 'confirm',
             'name': 'send',
-            'message': 'Do you want to take the break?'
+            'message': 'Confirm you want to make a break?'
         }
     ]
 
@@ -123,47 +101,41 @@ def main():
     """
     Simple CLI taking break app
     """
-    log("BonBreak", color="green", figlet=True)
-    log("Welcome break app", "purple")
+    log('This program was started on ' + time.ctime(), "blue")
+    log("B r e a k A p p", color="blue", figlet=True)
+    log("Welcome break app developed by bonbon", "green")
 
     res = inPuts()
     if res.get("send", False):
-        print('one break comes after two hours\n')
-        num_breaks = int(input('how many breaks do you want?: '))
-        break_duaration = int(
-            input('After how many hours do you need the break?: '))
-        breaks = 0
-        while (breaks < num_breaks):
-            uin = break_duaration
-            when_to_stop = abs(int(uin))
-
-            while when_to_stop >= 0:
-                m, s = divmod(when_to_stop, 60)
-                h, m = divmod(m, 60)
-                time_left = str(h).zfill(2) + ":" + \
-                    str(m).zfill(2) + ":" + str(s).zfill(2)
-                print(time_left + "\r", end="")
-                time.sleep(1)
-                when_to_stop -= 1
-                while when_to_stop == 0:
-                    accept = input("accept the break(Y/N): ")
-                    if accept.upper() == 'Y':
-                        log("when done with break hit enter\n", "green")
-                        webbrowser.open(
-                            "https://www.youtube.com/watch?v=m7Bc3pLyij0",
-                            new=0, autoraise=True)
-                    else:
-                        break
-        breaks += 1
         try:
-            response = take_break(res)
+            num_breaks = int(res.get("breaks"))
+            break_duaration = int(res.get("time"))
+            breaks = 0
+            while (num_breaks > breaks):
+                uin = break_duaration
+                when_to_stop = abs(int(uin))
+
+                while when_to_stop > 0:
+                    m, s = divmod(when_to_stop, 60)
+                    h, m = divmod(m, 60)
+                    time_left = str(h).zfill(2) + ":" + \
+                        str(m).zfill(2) + ":" + str(s).zfill(2)
+                    print(time_left + "\r", end="")
+                    time.sleep(1)
+                    when_to_stop -= 1
+                    while when_to_stop == 0:
+                        accept = confirm()
+                        if accept.get("send", False):
+                            log("when done with break hit enter\n", "green")
+                            webbrowser.open(
+                                "https://www.youtube.com/watch?v=m7Bc3pLyij0",
+                                new=0, autoraise=True)
+                            break
+                        else:
+                            break
+                num_breaks -= 1
         except Exception as e:
             raise Exception("An error occured: %s" % (e))
-
-        if response.status_code == 202:
-            log("Mail sent successfully", "blue")
-        else:
-            log("An error while trying to send", "red")
 
 
 if __name__ == '__main__':
